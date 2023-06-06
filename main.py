5cf3d641f4e74ac1ba406b0a742ceb7a43d7347c
track001 <45890450+track001@users.noreply.github.com>
2023-06-06 20:13:34 +0000

    Option to save encrypted CSV, fix age arguments based on DOD and DOB.

--- main.py
+++ main.py
@@ -2,40 +2,46 @@ import tkinter as tk
 from tkinter import filedialog, messagebox, Menu
 from datetime import date
 import pandas as pd
-import urllib.request
-
 
 def read_death_master_file(file_path):
     df = pd.read_csv(file_path)
     return df
 
-
-def filter_by_age(age):
-    today = pd.Timestamp.now().normalize()
-    filtered_df = verified_individuals[
-        ((today - pd.to_datetime(verified_individuals['date_of_birth'], errors='coerce')).dt.days / 365.25 <= age)
-        & (verified_individuals['date_of_death'].isnull()
-           | ((today - pd.to_datetime(verified_individuals['date_of_death'], errors='coerce')).dt.days / 365.25 <= age))
-    ]
-    preview_filtered_individuals(filtered_df)
-
-
-def preview_filtered_individuals(df):
-    verified_individuals_text.delete("1.0", tk.END)
-    verified_individuals_text.insert(tk.END, df.to_string(index=False))
-
+def calculate_age(date_of_birth, date_of_death):
+    today = date.today()
+    birth_date = pd.to_datetime(date_of_birth)
+    death_date = pd.to_datetime(date_of_death)
+    if death_date < today:
+        age = (death_date - birth_date) // pd.Timedelta(days=365.25)
+    else:
+        age = (today - birth_date) // pd.Timedelta(days=365.25)
+    return int(age)
 
 def display_verified_individuals():
-    today = pd.Timestamp.now().normalize()
-    verified_individuals['date_of_birth'] = pd.to_datetime(verified_individuals['date_of_birth'], errors='coerce')
-    verified_individuals['date_of_death'] = pd.to_datetime(verified_individuals['date_of_death'], errors='coerce')
-    verified_individuals['Age'] = ((today - verified_individuals['date_of_birth']).dt.days / 365.25).astype(int)
     verified_individuals_text.delete("1.0", tk.END)
-    verified_individuals_text.insert(tk.END, verified_individuals.to_string(index=False))
+    verified_individuals_text.insert(tk.END, "")
 
-    age_options = verified_individuals['Age'].unique()
-    age_options_text.delete("1.0", tk.END)
-    age_options_text.insert(tk.END, "\n".join(str(age) for age in age_options))
+    if 'verification' in verified_individuals.columns:
+        verified_df = verified_individuals[verified_individuals['verification'] == 'Verified']
+        verified_individuals_text.insert(tk.END, verified_df.to_string(index=False))
+    else:
+        verified_individuals_text.insert(tk.END, "No verified individuals found.")
+
+def calculate_age(date_of_birth, date_of_death, reference_date):
+    age = reference_date.year - date_of_birth.year
+    if reference_date < date_of_birth.replace(year=reference_date.year):
+        age -= 1
+    return age
+
+def display_age_options(df):
+    if 'date_of_birth' in df.columns and 'date_of_death' in df.columns:
+        today = date.today()
+        df['date_of_birth'] = pd.to_datetime(df['date_of_birth'])
+        df['date_of_death'] = pd.to_datetime(df['date_of_death'])
+        df['Age'] = df.apply(lambda row: calculate_age(row['date_of_birth'].date(), row['date_of_death'].date(), today), axis=1)
+        age_options = df['Age'].unique()
+        age_options_text.delete("1.0", tk.END)
+        age_options_text.insert(tk.END, "\n".join(str(age) for age in age_options))
 
 
 def download_csv_from_ntis(url, destination):
@@ -43,12 +49,16 @@ def download_csv_from_ntis(url, destination):
         urllib.request.urlretrieve(url, destination)
         global verified_individuals
         verified_individuals = read_death_master_file(destination)
+        verified_individuals['Age'] = verified_individuals.apply(lambda row: calculate_age(row['date_of_birth'], row['date_of_death']), axis=1)
         messagebox.showinfo("Success", "CSV file downloaded and read successfully!")
         display_verified_individuals()
+        display_age_options(verified_individuals)
     except pd.errors.ParserError as pe:
         messagebox.showerror("Error", f"Error parsing CSV file: {str(pe)}")
     except Exception as e:
         messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+# Rest of the code remains the same
 
 
 def open_file_dialog():
@@ -59,9 +69,11 @@ def open_file_dialog():
             verified_individuals = read_death_master_file(file_path)
             messagebox.showinfo("Success", "CSV file read successfully!")
             display_verified_individuals()
+            display_age_options(verified_individuals)
         except Exception as e:
             messagebox.showerror("Error", f"An error occurred: {str(e)}")
-
+    else:
+        messagebox.showinfo("Information", "No file selected.")
 
 def fetch_from_ntis():
     menu = Menu(fetch_from_ntis_button, tearoff=0)
@@ -69,7 +81,6 @@ def fetch_from_ntis():
     menu.add_command(label="Monthly", command=fetch_monthly)
 
     fetch_from_ntis_button.configure(menu=menu)
-
 
 def fetch_weekly():
     url = 'https://dmf.ntis.gov/weekly/'
@@ -83,7 +94,6 @@ def fetch_weekly():
         except Exception as e:
             messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
-
 def fetch_monthly():
     url = 'https://dmf.ntis.gov/monthly/'
     today = date.today().strftime("%m%d%Y")
@@ -96,6 +106,17 @@ def fetch_monthly():
         except Exception as e:
             messagebox.showerror("Error", f"An error occurred: {str(e)}")
 
+def save_encrypted_csv():
+    today = date.today().strftime("%m%d%Y")
+    destination = filedialog.asksaveasfilename(defaultextension=".csv",
+                                               filetypes=(("CSV files", "*.csv"), ("All files", "*.*")),
+                                               initialfile=f"VerifiedIndividuals{today}")
+
+    if destination:
+        encrypted_df = verified_individuals.copy()
+        encrypted_df['ssn'] = 'XXX-XX-' + encrypted_df['ssn'].str[-4:]
+        encrypted_df.to_csv(destination, index=False)
+        messagebox.showinfo("Success", "Encrypted CSV file saved successfully!")
 
 window = tk.Tk()
 window.title("Death Master File Viewer")
@@ -111,32 +132,17 @@ fetch_from_ntis_button.pack(pady=5)
 
 fetch_from_ntis()
 
-age_label = tk.Label(window, text="Filter by Age:")
-age_label.pack()
-
-age_entry = tk.Entry(window, width=10)
-age_entry.pack()
-
-
-def filter_button_callback():
-    age = age_entry.get()
-    if age.isdigit():
-        filter_by_age(int(age))
-    else:
-        messagebox.showerror("Error", "Please enter a valid age (numeric value).")
-
-
-filter_button = tk.Button(window, text="Filter", command=filter_button_callback)
-filter_button.pack(pady=5)
-
-view_all_button = tk.Button(window, text="View All", command=display_verified_individuals)
-view_all_button.pack(pady=5)
-
 age_options_label = tk.Label(window, text="Age Options:")
 age_options_label.pack()
 
 age_options_text = tk.Text(window, height=5, width=80)
 age_options_text.pack(pady=10)
+
+view_verified_button = tk.Button(window, text="View Verified", command=display_verified_individuals)
+view_verified_button.pack(pady=5)
+
+save_encrypted_csv_button = tk.Button(window, text="Save Encrypted CSV", command=save_encrypted_csv)
+save_encrypted_csv_button.pack(pady=5)
 
 verified_individuals_text = tk.Text(window, height=20, width=100)
 verified_individuals_text.pack(pady=10)
